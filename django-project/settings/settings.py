875fc789853954dc15e159184f0e6ac815f3e2e1
Jason Yeomans <jyeo098@gmail.com>
2015-12-27 23:48:39 -0500

    templates dir

--- django-project/settings/settings.py
+++ django-project/settings/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
