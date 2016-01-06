d49648025f9868d01e85d073760cfe9b3c57733c
Jason Yeomans <jyeo098@gmail.com>
2016-01-05 21:41:53 -0500

    post formatting

--- django-project/static/js/app/init.js
+++ django-project/static/js/app/init.js
@@ -1,8 +1,16 @@
-(function($){
-  $(function(){
+(function($) {
+  $(function() {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.post-body img').each(function() {
+      var $ths = $(this);
+      if ($ths.css('float') === 'left') {
+        $ths.addClass('fleft-padding');
+      } else if ($ths.css('float') === 'right') {
+        $ths.addClass('fright-padding');
+      }
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
